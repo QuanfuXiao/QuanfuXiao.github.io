c4243dfcb369404d1fe4ccb8a3c0b6147ebf5ee6
sunsetXiao <quanfuxiao@microsoft.com>
2023-07-19 16:13:53 +0800

    fix

--- src/taskpane/taskpane.js
+++ src/taskpane/taskpane.js
@@ -13,7 +13,7 @@ Office.onReady((info) => {
   }
 });
 
-export async function run() {
+async function run() {
   return Word.run(async (context) => {
     /**
      * Insert your Word code here
@@ -28,3 +28,5 @@ export async function run() {
     await context.sync();
   });
 }
+
+module.exports = { run }
