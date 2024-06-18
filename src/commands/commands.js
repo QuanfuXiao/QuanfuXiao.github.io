e77914ebdfeebee6df7a219b8395d561ad9f0c5e
quanfuxiao <quanfuxiao@microsoft.com>
2024-06-18 13:56:47 +0800

    Update commands.js

--- src/commands/commands.js
+++ src/commands/commands.js
@@ -5,15 +5,38 @@
 
 /* global global, Office, self, window */
 
-Office.onReady(() => {
-  // If needed, Office.js is ready to be called
+Office.onReady((info) => {
+  if (info.host === Office.HostType.Outlook) {
+    // Register the function with Office.
+    Office.actions.associate("action", actionOutlook);
+  } else if (info.host === Office.HostType.Excel) {
+    // Register the function with Office.
+    Office.actions.associate("action", actionExcel);
+  } else if (info.host === Office.HostType.PowerPoint) {
+    // Register the function with Office.
+    Office.actions.associate("action", actionPowerPoint);
+  } else if (info.host === Office.HostType.Word) {
+    // Register the function with Office.
+    Office.actions.associate("action", actionWord);
+  }
 });
 
-/**
- * Shows a notification when the add-in command is executed.
- * @param event {Office.AddinCommands.Event}
- */
-async function action(event) {
+function actionOutlook(event) {
+  const message = {
+    type: Office.MailboxEnums.ItemNotificationMessageType.InformationalMessage,
+    message: "Performed action.",
+    icon: "Icon.80x80",
+    persistent: true,
+  };
+
+  // Show a notification message.
+  Office.context.mailbox.item.notificationMessages.replaceAsync("ActionPerformanceNotification", message);
+
+  // Be sure to indicate when the add-in command function is complete.
+  event.completed();
+}
+
+async function actionExcel(event) {
   try {
     await Excel.run(async (context) => {
         const range = context.workbook.getSelectedRange();
@@ -29,17 +52,34 @@ async function action(event) {
   event.completed();
 }
 
-function getGlobal() {
-  return typeof self !== "undefined"
-    ? self
-    : typeof window !== "undefined"
-    ? window
-    : typeof global !== "undefined"
-    ? global
-    : undefined;
+async function actionWord(event) {
+  try {
+    await Word.run(async (context) => {
+      const paragraph = context.document.body.insertParagraph("Hello World", Word.InsertLocation.end);
+      paragraph.font.color = "blue";
+      await context.sync();
+    });
+  } catch (error) {
+    // Note: In a production add-in, notify the user through your add-in's UI.
+    console.error(error);
+  }
+
+  // Be sure to indicate when the add-in command function is complete
+  event.completed();
 }
 
-const g = getGlobal();
+async function actionPowerPoint(event) {
+  try {
+    await PowerPoint.run(async (context) => {
+      const options = { coercionType: Office.CoercionType.Text };
+      await Office.context.document.setSelectedDataAsync(" ", options);
+      await Office.context.document.setSelectedDataAsync("Hello World!", options);
+    });
+  } catch (error) {
+    // Note: In a production add-in, notify the user through your add-in's UI.
+    console.error(error);
+  }
 
-// The add-in command functions need to be available in global scope
-g.action = action;
+  // Be sure to indicate when the add-in command function is complete
+  event.completed();
+}
